2f49dbc9296c75acae158e295b0580bfe7732f0b
Achal Singhal <achals2002@gmail.com>
2022-11-09 22:02:20 +0530

    floor call implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-UP = 0
-DOWN = 1
+IDLE = 0
+UP = 1
+DOWN = 2
 
 class Building():
     def __init__(self, floor_count = 6, elevator_count = 3):
@@ -7,10 +8,20 @@ class Building():
         self.floors = list(range(1, floor_count + 1))
         self.elevators = []
         for i in range(elevator_count):
-            self.elevators.append(Elevator())
+            self.elevators.append(Elevator(8))
     
     def call(self, floor, direction):
-        pass
+        dist = []
+        for elevator in self.elevators:
+            dist.append(abs(elevator.cur_floor - floor))
+        self.elevators[dist[dist.index(min(dist))]].call(floor)
+                
 
 class Elevator():
-    pass
+    def __init__(self, max_capacity = 8):
+        self.cur_floor = 0
+        self.dir = IDLE
+        self.max_capacity = max_capacity
+    
+    def call(self, floor):
+        pass
